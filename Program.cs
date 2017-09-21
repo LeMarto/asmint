9ff20b02a19b9bb5741c112c4d5ca4d8e132144a
Martin Ignacio Alonso <martin.ignacio.alonso@gmail.com>
2017-09-21 20:00:55 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asmint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
