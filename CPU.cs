a7e9df260ce2d8a85c27a7fc474a57025c140544
Alonso <martin.ignacio.alonso@gmail.com>
2017-09-22 18:21:49 -0300

    test commit

--- CPU.cs
+++ CPU.cs
@@ -2,14 +2,24 @@ using System;
 
 namespace asmint
 {
+    public class Instruction
+    {
+        public byte code;  //opcode
+        public byte meta;  //register/memory/value
+        public byte op1;
+        public byte op2;
+
+        public Instruction(byte code, byte meta, byte op1, byte op2)
+        {
+            this.code = code;
+            this.meta = meta;
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
     public class CPU
     {
-        public class Instruction
-        {
-            byte header;  //opcode and info first 4 bytes opcode, last 4, metadata of op1 and op 2 param
-            byte op1;
-            byte op2;
-        }
+
         public enum opcodes
         {
             mov,
@@ -46,9 +56,28 @@ namespace asmint
         public byte ax, bx, cx, dx, sp, bp, si, di, ip, cs, ds, es, fs, gs, ss, flags;
         public byte[,] memory = new byte[255,255];
 
+        private void LoadInstructionToMemory(Instruction ins)
+        {
+            memory[cs,ip] = ins.code;
+            memory[cs,ip+1] = ins.meta;
+            memory[cs,ip+2] = ins.op1;
+            memory[cs,ip+3] = ins.op2;
+            
+            ip += 4;
+            if (ip > 255)
+            {
+                ip = 0;
+                cs +=1;
+
+                if (cs > 255)
+                    Console.WriteLine("Error! Out of Memory!");
+            }
+        }
         public CPU()
         {
-            Console.WriteLine("Hello World!");
+            //set code segment and instruction pointer to top of memory
+            ip = 0;
+            cs = 0;
         }
     }
 }
